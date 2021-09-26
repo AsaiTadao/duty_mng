a20971909e6e9f379c518e278be89e9ad6dbe7cc
pineblue <bluepine810@hotmail.com>
2021-09-27 02:29:14 +0800

    add master-managment page

--- resources/js/Admin/components/Master.vue
+++ resources/js/Admin/components/Master.vue
@@ -1,19 +1,56 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="col-md-6">
-                            <button type="button" class="btn btn-sm btn-outline-primary mx-2" @click="getResults(this.currentDate)">
-                                今月
-                            </button>
-                            <button type="button" class="btn btn-sm btn-outline-primary mx-2" @click="getResults(this.currentDate)">
-                                今月
-                            </button>
-                        </div>
-                    </div>
+        <div class="row justify-content-center my-5">
+            <div class="col-md-3">
+                <div type="button" class="border-radius-15 border-color-blue border-style-solid border-width-thin background-grey py-1" @click="getResults(this.currentDate)">
+                    <h3 class="text-white text-center mb-0 mt-2">
+                        事業所マスタ
+                    </h3>
+                    <div class="text-white text-center mb-2">事業所の登録｜編集｜削除</div>
                 </div>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-3">
+                <div type="button" class="border-radius-15 border-color-orange border-style-solid border-width-thin background-grey py-1" @click="getResults(this.currentDate)">
+                    <h3 class="text-white text-center mb-0 mt-2">
+                        社員マスタ
+                    </h3>
+                    <div class="text-white text-center mb-2">社員情報の登録｜編集｜削除</div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row justify-content-center my-5">
+            <div class="col-md-3">
+                <div type="button" class="border-radius-15 border-color-purple border-style-solid border-width-thin background-grey py-1" @click="getResults(this.currentDate)">
+                    <h3 class="text-white text-center mb-0 mt-2">
+                        時間帯マスタ
+                    </h3>
+                    <div class="text-white text-center mb-2">時間帯の登録｜編集｜削除</div>
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-3">
+                <div type="button" class="border-radius-15 border-color-orange border-style-solid border-width-thin background-grey py-1" @click="getResults(this.currentDate)">
+                    <h3 class="text-white text-center mb-0 mt-2">
+                        社員マスタ
+                    </h3>
+                    <div class="text-white text-center mb-2">社員情報の登録｜編集｜削除</div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row justify-content-center my-5">
+            <div class="col-md-3">
+                <div type="button" class="border-radius-15 border-color-blue border-style-solid border-width-thin background-grey py-1" @click="getResults(this.currentDate)">
+                    <h3 class="text-white text-center mb-0 mt-2">
+                        事業所マスタ
+                    </h3>
+                    <div class="text-white text-center mb-2">事業所の登録｜編集｜削除</div>
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-3">
             </div>
         </div>
     </div>
