5b2b25f9bec7e2a737e1f2ec558bc75e37f4e731
devcrazy <devcrazy@hotmail.com>
2021-12-05 04:17:13 +0800

    add working hour apis

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\API\V1\ConstantController;
 use App\Http\Controllers\API\V1\OfficeController;
 use App\Http\Controllers\API\V1\RegionController;
 use App\Http\Controllers\API\V1\VacationReasonController;
+use App\Http\Controllers\API\V1\WorkingHourController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -29,26 +30,30 @@ Route::namespace('App\\Http\\Controllers\\API\V1')->prefix('/v1')->group(functio
 
     Route::middleware(['auth:api'])->group(function () {
         Route::get('/me', [AuthController::class, 'me']);
+        Route::middleware(['can:admin-only'])->group(function() {
+            Route::get('/office-master', [OfficeController::class, 'get'])->name('office_master.get');
+            Route::post('/office-master', [OfficeController::class, 'create'])->name('office_master.create');
+            Route::put('/office-master/{office}', [OfficeController::class, 'update'])->name('office_master.update');
+            Route::delete('/office-master/{office}', [OfficeController::class, 'delete'])->name('office_master.delete');
 
-        Route::get('/office-master', [OfficeController::class, 'get'])->name('office_master.get');
-        Route::post('/office-master', [OfficeController::class, 'create'])->name('office_master.create');
-        Route::put('/office-master/{office}', [OfficeController::class, 'update'])->name('office_master.update');
-        Route::delete('/office-master/{office}', [OfficeController::class, 'delete'])->name('office_master.delete');
-
-        Route::get('/office-master/{office}/scheduled-working', [OfficeController::class, 'getScheduleWorkings'])->name('office_master.scheduled_working.get');
-        Route::post('/office-master/{office}/scheduled-working', [OfficeController::class, 'saveScheduleWorking'])->name('office_master.scheduled_working.save');
+            Route::get('/office-master/{office}/scheduled-working', [OfficeController::class, 'getScheduleWorkings'])->name('office_master.scheduled_working.get');
+            Route::post('/office-master/{office}/scheduled-working', [OfficeController::class, 'saveScheduleWorking'])->name('office_master.scheduled_working.save');
 
 
-        Route::get('/region', [RegionController::class, 'get'])->name('region.get');
-        Route::post('/region', [RegionController::class, 'create'])->name('region.create');
-        Route::post('/region/{region}', [RegionController::class, 'update'])->name('region.update');
-        Route::delete('/region/{region}', [RegionController::class, 'delete'])->name('region.delete');
+            Route::get('/region', [RegionController::class, 'get'])->name('region.get');
+            Route::post('/region', [RegionController::class, 'create'])->name('region.create');
+            Route::post('/region/{region}', [RegionController::class, 'update'])->name('region.update');
+            Route::delete('/region/{region}', [RegionController::class, 'delete'])->name('region.delete');
 
-        Route::get('/reason-for-vacation', [VacationReasonController::class, 'get'])->name('vacation_reason.get');
-        Route::post('/reason-for-vacation', [VacationReasonController::class, 'create'])->name('vacation_reason.create');
-        Route::put('/reason-for-vacation/status/{reasonForVacation}', [VacationReasonController::class, 'updateStatus']);
-        Route::post('/reason-for-vacation/{reasonForVacation}', [VacationReasonController::class, 'update']);
-        Route::delete('/reason-for-vacation/{reasonForVacation}', [VacationReasonController::class, 'delete']);
+            Route::get('/reason-for-vacation', [VacationReasonController::class, 'get'])->name('vacation_reason.get');
+            Route::post('/reason-for-vacation', [VacationReasonController::class, 'create'])->name('vacation_reason.create');
+            Route::put('/reason-for-vacation/status/{reasonForVacation}', [VacationReasonController::class, 'updateStatus']);
+            Route::post('/reason-for-vacation/{reasonForVacation}', [VacationReasonController::class, 'update']);
+            Route::delete('/reason-for-vacation/{reasonForVacation}', [VacationReasonController::class, 'delete']);
 
+            Route::get('/working-hours', [WorkingHourController::class, 'get']);
+            Route::post('/working-hours', [WorkingHourController::class, 'create']);
+            Route::put('/working-hours/{workingHour}', [WorkingHourController::class, 'update']);
+        });
     });
 });
