faa6e4c28bff834ba8bd097ed9bc2efbdfaf9353
pineblue <bluepine810@hotmail.com>
2022-01-14 23:01:56 +0800

    fix デザイン調整 #7253

--- resources/js/pages/AttendStatus/ApplicationForm.vue
+++ resources/js/pages/AttendStatus/ApplicationForm.vue
@@ -154,7 +154,7 @@ import { showSuccess } from '../../helpers/error';
                 var day = today.getDate();
                 return month + "月" + day + "日 "
                 + today.getHours() + ":"
-                + today.getMinutes();
+                + ('0' + today.getMinutes()).slice(-2);
             },
             getDate(index) {
                 const date = this.month + '-' + ('0' + index).slice(-2);
