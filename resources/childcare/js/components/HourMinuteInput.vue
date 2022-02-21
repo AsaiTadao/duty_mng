14a3d892ddd2f76cd55c896f17b7756c8b61531c
devcrazy <devcrazy@hotmail.com>
2022-02-21 21:24:57 +0800

    enable defecation 0

--- resources/childcare/js/components/HourMinuteInput.vue
+++ resources/childcare/js/components/HourMinuteInput.vue
@@ -1,34 +1,35 @@
 <template>
-    <div class="home-minute-input-wrapper">
-        <div class="home-minute-input">
-            <input type="number" class="form-control" min="0" max="24" :value="hour" :class="{'is-invalid' : error}" @input="inputHour">
+    <div class="home-minute-input-container">
+        <div class="home-minute-input-wrapper">
+            <div class="home-minute-input">
+                <input type="number" class="form-control" min="0" max="24" :value="hour" :class="{'is-invalid' : error}" @input="inputHour">
+            </div>
+            :
+            <div class="home-minute-input">
+                <input type="number" class="form-control" min="0" max="60" :value="minute" :class="{'is-invalid' : error}" @input="inputMinute">
+            </div>
         </div>
-        :
-        <div class="home-minute-input">
-            <input type="number" class="form-control" min="0" max="60" :value="minute" :class="{'is-invalid' : error}" @input="inputMinute">
+        <div v-if="error" class="home-minute-input-error">
+            {{ error }}
         </div>
     </div>
 </template>
 <script>
 export default {
     props: {
-        value: String
+        value: String,
+        error: String
     },
     computed: {
         hour() {
-            if (!this.value) return '00';
+            if (!this.value) return '';
             let arr = this.value.split(':');
             return arr[0];
         },
         minute() {
-            if (!this.value) return '00';
+            if (!this.value) return '';
             let arr = this.value.split(':');
             return arr[1];
-        }
-    },
-    data() {
-        return {
-            error: false,
         }
     },
     methods: {
@@ -41,20 +42,26 @@ export default {
         },
         inputMinute(e) {
             let minute = e.target.value;
-            if (!minute) {
-                minute = '00';
-            }
             this.$emit('input', this.hour + ':' + minute);
         }
     }
 }
 </script>
 <style scoped>
+.home-minute-input-container {
+    display: flex;
+    flex-direction: column;
+    column-gap: 5px;
+}
 .home-minute-input-wrapper {
     display: flex;
 }
 .home-minute-input {
     margin-right: 5px;
     margin-left: 5px;
+}
+.home-minute-input-error {
+    margin-top: 5px;
+    color: #f00;
 }
 </style>
