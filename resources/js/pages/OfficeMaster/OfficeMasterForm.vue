bfe71af228ee8519a41aefd7dbec0d3a14ac9010
pineblue <bluepine810@hotmail.com>
2022-02-24 21:16:45 +0800

    add officemaster edit form

--- resources/js/pages/OfficeMaster/OfficeMasterForm.vue
+++ resources/js/pages/OfficeMaster/OfficeMasterForm.vue
@@ -50,6 +50,127 @@
                     </span>
                 </div>
             </div>
+
+            <div class="form-group">
+                <div class="form-row">
+                    <div class="col-md-1">
+                        <input type="radio" name="kind" :value="1" v-model="nurseOffice">
+                        <label class="ml-auto">保育園</label>
+                    </div>
+                    <div class="col-md-1">
+                        <input type="radio" name="kind" :value="0" v-model="nurseOffice">
+                        <label class="ml-auto">その他</label>
+                    </div>
+                </div>
+            </div>
+            <template v-if="nurseOffice">
+            <div class="form-group">
+                <div class="form-row">
+                    <div class="col-md-4">
+                        <label>以下の項目は保育園のみ登録</label>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col-md-2">
+                        開所時間：
+                    </div>
+                    <div class="col-md-1">
+                        <input type="number" class="form-control" min="0" max="24" :class="{'is-invalid' : errors.startTime}" @change="errors.startTime = null">
+                        <span v-if="errors.startTime" class="error invalid-feedback">
+                            {{ errors.startTime }}
+                        </span>
+                    </div>
+                    :
+                    <div class="col-md-1">
+                        <input type="number" class="form-control" min="0" max="60" :class="{'is-invalid' : errors.startTime}" @change="errors.startTime = null">
+                        <span v-if="errors.startTime" class="error invalid-feedback">
+                            {{ errors.startTime }}
+                        </span>
+                    </div>
+                    ~
+                    <div class="col-md-1">
+                        <input type="number" class="form-control" min="0" max="24" :class="{'is-invalid' : errors.endTime}" @change="errors.endTime = null">
+                        <span v-if="errors.endTime" class="error invalid-feedback">
+                            {{ errors.endTime }}
+                        </span>
+                    </div>
+                    :
+                    <div class="col-md-1">
+                        <input type="number" class="form-control" min="0" max="60" :class="{'is-invalid' : errors.endTime}" @change="errors.endTime = null">
+                        <span v-if="errors.endTime" class="error invalid-feedback">
+                            {{ errors.endTime }}
+                        </span>
+                    </div>
+                </div>
+                <div class="form-row mt-1">
+                    <div class="col-md-2">
+                        定員数：
+                    </div>
+                    <div class="col-md-1">
+                        <div class="d-flex">
+                        <input type="number" class="form-control" min="0" max="24" :class="{'is-invalid' : errors.startTime}" @change="errors.startTime = null">
+                        名
+                        <span v-if="errors.startTime" class="error invalid-feedback">
+                            {{ errors.startTime }}
+                        </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row mt-1">
+                    <div class="col-md-2">
+                        適正職員数：<br>
+                        (一人当たり)
+                    </div>
+                    <div class="col-md-2">
+                        <div class="d-flex mb-1 align-middle">
+                            <div class="mr-1 align-self-center">0歳児</div>
+                            <input type="number" class="form-control" style="width:50%" min="0" max="1000" :class="{'is-invalid' : errors.startTime}" @change="errors.startTime = null">
+                            <div class="ml-1 align-self-center">名</div>
+                        </div>
+                        <div class="d-flex">
+                            <div class="mr-1 align-self-center">3歳児</div>
+                            <input type="number" class="form-control" style="width:50%" min="0" max="1000" :class="{'is-invalid' : errors.startTime}" @change="errors.startTime = null">
+                            <div class="ml-1 align-self-center">名</div>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="d-flex mb-1">
+                            <div class="mr-1 align-self-center">1歳児</div>
+                            <input type="number" class="form-control" style="width:50%" min="0" max="1000" :class="{'is-invalid' : errors.startTime}" @change="errors.startTime = null">
+                            <div class="ml-1 align-self-center">名</div>
+                        </div>
+                        <div class="d-flex mb-1">
+                            <div class="mr-1 align-self-center">4歳児</div>
+                            <input type="number" class="form-control" style="width:50%" min="0" max="1000" :class="{'is-invalid' : errors.startTime}" @change="errors.startTime = null">
+                            <div class="ml-1 align-self-center">名</div>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="d-flex mb-1">
+                            <div class="mr-1 align-self-center">2歳児</div>
+                            <input type="number" class="form-control" style="width:50%" min="0" max="1000" :class="{'is-invalid' : errors.startTime}" @change="errors.startTime = null">
+                            <div class="ml-1 align-self-center">名</div>
+                        </div>
+                        <div class="d-flex">
+                            <div class="mr-1 align-self-center">5歳児</div>
+                            <input type="number" class="form-control" style="width:50%" min="0" max="1000" :class="{'is-invalid' : errors.startTime}" @change="errors.startTime = null">
+                            <div class="ml-1 align-self-center">名</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row mt-1">
+                    <div class="col-md-2">
+                        事業種別：
+                    </div>
+                    <div class="col-md-6">
+                        <select class="form-control">
+                            <option>全業主導型保育事業所</option>
+                            <option></option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            </template>
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
@@ -97,7 +218,8 @@ import { showSuccess } from '../../helpers/error';
                     name: '',
                     number: '',
                     restDeductionId: ''
-                }
+                },
+                nurseOffice: 0,
             }
         },
         methods: {
