71c48059395a51ab4368661bd708a7725ca4eaea
devcrazy <devcrazy@hotmail.com>
2022-02-28 23:11:38 +0800

    change validation

--- app/Http/Requests/Child/ContactBook345SchoolRequest.php
+++ app/Http/Requests/Child/ContactBook345SchoolRequest.php
@@ -10,10 +10,10 @@ class ContactBook345SchoolRequest extends FormRequest {
     {
         return [
             'date'              =>  ['required', 'date_format:Y-m-d'],
-            'weather'           =>  ['nullable', 'string'],
+            'weather'           =>  ['required', 'string'],
             'nurse_name'        =>  ['nullable', 'string'],
             'guardian'          =>  ['nullable', 'string'],
-            'contact_0_school'  =>  ['required', 'string']
+            'contact_0_school'  =>  ['nullable', 'string']
         ];
     }
 }
