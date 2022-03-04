4140a1ed3196be0b8e75ad64eb315bcb87b3f82c
devcrazy <devcrazy@hotmail.com>
2022-03-04 23:06:29 +0800

    [#7641] fix mail history order by

--- app/Http/Controllers/API/V1/Child/MailController.php
+++ app/Http/Controllers/API/V1/Child/MailController.php
@@ -44,7 +44,7 @@ class MailController extends BaseController
         } else {
             $size = $data['per_page'];
         }
-        $mails = MailJobHistory::with('office')->where(['office_id' => $user->office_id])->paginate($size);
+        $mails = MailJobHistory::with('office')->where(['office_id' => $user->office_id])->orderBy('id', 'desc')->paginate($size);
         return $this->sendResponse([
             'data'  =>  $mails->items(),
             'total' =>  $mails->total(),
