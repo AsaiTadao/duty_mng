8da895fc25d9a66e34200cb5fec408f0351ea455
AsaiTadao <blackhole.rsb@gmail.com>
2022-03-14 23:05:52 +0800

    fix contact book validation

--- app/Http/Requests/Child/ContactBook12SchoolRequest.php
+++ app/Http/Requests/Child/ContactBook12SchoolRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Child;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Models\ContactBook;
 
 class ContactBook12SchoolRequest extends FormRequest {
 
--- app/Http/Requests/Child/ContactBook345SchoolRequest.php
+++ app/Http/Requests/Child/ContactBook345SchoolRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Child;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Models\ContactBook;
 
 class ContactBook345SchoolRequest extends FormRequest {
 
