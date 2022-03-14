4594fef288cea6e1a3aa4046efe38b5d08c6adda
pineblue <bluepine810@hotmail.com>
2022-03-14 12:07:27 +0800

    add office_name to the child edit page

--- resources/childcare/js/pages/ChildrenDetail.vue
+++ resources/childcare/js/pages/ChildrenDetail.vue
@@ -6,7 +6,7 @@
                     <div class="card-header calendar-title">
                         <div class="row">
                             <div class="col-md-3">
-                                <h3 class="card-title mb-0">保育システム</h3>
+                                <h3 class="card-title mb-0">{{session.office.name}}</h3>
                             </div>
                             <div class="col-md-5"></div>
                             <div class="col-md-4">
