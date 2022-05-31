c4cd168f3e34bb73c393737685cf4223fa7496a1
AsaiTadao <blackhole.rsb@gmail.com>
2022-05-31 22:45:17 +0800

    Merge branch 'master' of https://gitlab.com/seiritu/lktotalmanagementsystem

--- app/Exports/ChildDiaryExport.php
+++ app/Exports/ChildDiaryExport.php
@@ -36,30 +36,30 @@ class ChildDiaryExport implements WithEvents
                 $sheet = $event->writer->getSheetByIndex(0);
 
                 $sheet->setCellValue('a1', $this->office->name);
-                $sheet->setCellValue('b5', $this->getClassLabel($this->childrenClassId));
+                $sheet->setCellValue('a4', $this->getClassLabel($this->childrenClassId));
 
                 $date = Carbon::parse($this->diary->date);
                 $date->settings(['formatFunction' => 'translatedFormat']);
-                $sheet->setCellValue('i3', $date->format('Y年 F jS（l）'));
-                $sheet->setCellValue('j4', $this->diary->weather);
+                $sheet->setCellValue('f1', $date->format('Y年 F jS（l）'));
+                $sheet->setCellValue('f2', '天気：' . $this->diary->weather);
 
-                $sheet->setCellValue('a9', $this->stat['all']);
-                $sheet->setCellValue('c9', $this->stat['attend']);
-                $sheet->setCellValue('e9', $this->stat['absent']);
+                $sheet->setCellValue('a7', $this->stat['all']);
+                $sheet->setCellValue('b7', $this->stat['attend']);
+                $sheet->setCellValue('c7', $this->stat['absent']);
 
-                $sheet->setCellValue('g8', $this->diary->reason_for_absence);
-                $sheet->setCellValue('r8', $this->diary->special_note);
+                $sheet->setCellValue('e7', $this->diary->reason_for_absence);
+                $sheet->setCellValue('h7', $this->diary->special_note);
 
-                $sheet->setCellValue('d16', $this->diary->aim);
-                $sheet->setCellValue('p16', $this->diary->activity_content);
-
-
-                $sheet->setCellValue('d20', $this->diary->consideration);
-                $sheet->setCellValue('d22', $this->diary->evaluation_reflection);
+                $sheet->setCellValue('e12', $this->diary->aim);
+                $sheet->setCellValue('h12', $this->diary->activity_content);
 
 
-                $sheet->setCellValue('a29', $this->diary->health_status);
-                $sheet->setCellValue('a37', $this->diary->remarks);
+                $sheet->setCellValue('e13', $this->diary->consideration);
+                $sheet->setCellValue('e14', $this->diary->evaluation_reflection);
+
+
+                $sheet->setCellValue('a18', $this->diary->health_status);
+                $sheet->setCellValue('a26', $this->diary->remarks);
 
             }
         ];
