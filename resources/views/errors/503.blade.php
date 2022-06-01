a73f3d0d9ba032602036ce568e1a9212fb6ecb18
genie <genie.it.ninja@gmail.com>
2022-06-01 16:03:06 +0900

    メンテナンス画面 仮コミット

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>メンテナンス中</title>
+</head>
+<div class="login-page">
+    <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-header text-center p-2">
+                <a href="#" class="h1"
+                ><img src="/images/logo1.png" style="width:90%; height:90%"
+                    /></a>
+            </div>
+            <div class="card-body">
+                <h3>ただいまメンテナンス中です</h3>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+</div>
+</html>
